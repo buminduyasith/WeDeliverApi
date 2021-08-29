6c3b26a519dc4517b0e80a8b1248abd6e17e0c41
Bumindu Yasith <bumiduyasith@gmail.com>
2021-08-29 22:36:41 +0530

    add identity tables

--- Application/wedeliver.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Application/wedeliver.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,8 @@ using wedeliver.Domain.Common;
 
 namespace wedeliver.Infrastructure.Persistence
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext
+        //DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
