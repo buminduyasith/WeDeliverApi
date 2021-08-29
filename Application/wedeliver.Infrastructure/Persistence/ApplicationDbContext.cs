77ff1d7c346cb240401ee4f882d96f97b0634998
Bumindu Yasith <bumiduyasith@gmail.com>
2021-08-29 22:28:40 +0530

    add identity db

--- Application/wedeliver.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Application/wedeliver.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,7 @@ using wedeliver.Domain.Common;
 
 namespace wedeliver.Infrastructure.Persistence
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext   //DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
