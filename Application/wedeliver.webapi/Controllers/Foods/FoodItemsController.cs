181c88f65f140902161660ae7ea22f46f62a1eb8
Bumindu Yasith <bumiduyasith@gmail.com>
2021-09-06 09:27:38 +0530

    change connectionstring

--- Application/wedeliver.webapi/Controllers/Foods/FoodItemsController.cs
+++ Application/wedeliver.webapi/Controllers/Foods/FoodItemsController.cs
@@ -22,13 +22,13 @@ namespace wedeliver.webapi.Controllers.Foods
             return Ok(result);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> AddFoodItem()
-        {
-            var query = new GetFoodListQuery();
-            var result = await Mediator.Send(query);
-            return Ok(result);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> AddFoodItem()
+        //{
+        //    var query = new GetFoodListQuery();
+        //    var result = await Mediator.Send(query);
+        //    return Ok(result);
+        //}
 
 
 
