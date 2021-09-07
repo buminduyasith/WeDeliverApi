c352cf53e5378ec9bd9793ca5c909f8faa66b068
Bumindu Yasith <bumiduyasith@gmail.com>
2021-09-07 13:06:52 +0530

    migration updated

---
+++ Application/wedeliver.Infrastructure/Migrations/20210907073453_clientFeildTypeChanged.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wedeliver.Infrastructure.Migrations
+{
+    public partial class clientFeildTypeChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
