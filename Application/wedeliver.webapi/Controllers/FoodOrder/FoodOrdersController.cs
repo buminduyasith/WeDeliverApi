37b1c5ed4d5b0078ea1ddb8a1695f2649f712151
Bumindu Yasith <bumiduyasith@gmail.com>
2021-09-20 13:13:13 +0530

    add get food order by restaurant id controller

--- Application/wedeliver.webapi/Controllers/FoodOrder/FoodOrdersController.cs
+++ Application/wedeliver.webapi/Controllers/FoodOrder/FoodOrdersController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using wedeliver.Application.Features.FoodOrders.Commands.CreateFoodOrder;
 using wedeliver.Application.Features.FoodOrders.Commands.UpdateFoodOrderStatus;
+using wedeliver.Application.Features.FoodOrders.Queries.GetFoodOrderById;
 using wedeliver.webapi.Controllers.Base;
 
 namespace wedeliver.webapi.Controllers.FoodOrder
@@ -29,6 +30,22 @@ namespace wedeliver.webapi.Controllers.FoodOrder
         {
             var result = await Mediator.Send(updateFoodOrderStatusCommand);
             return Ok(result);
+        }
+
+
+        [HttpGet(("restaurant/{id}/foodorders"), Name = "GetAllFoodOrdersByRestaurantId")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllFoodOrdersByRestaurantId(GetFoodOrderByRestaurantIdQuery getFoodOrderByRestaurantIdQuery)
+        {
+            var result = await Mediator.Send(getFoodOrderByRestaurantIdQuery);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+           
         }
     }
 }
