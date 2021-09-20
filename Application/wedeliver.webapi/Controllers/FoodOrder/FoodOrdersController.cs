856f50b4a7d5bbd3c52e8e222542ce3515886590
Bumindu Yasith <bumiduyasith@gmail.com>
2021-09-20 19:16:55 +0530

    create GetSpecificFoodOrderByClient controller

--- Application/wedeliver.webapi/Controllers/FoodOrder/FoodOrdersController.cs
+++ Application/wedeliver.webapi/Controllers/FoodOrder/FoodOrdersController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using wedeliver.Application.Features.FoodOrders.Commands.CreateFoodOrder;
 using wedeliver.Application.Features.FoodOrders.Commands.UpdateFoodOrderStatus;
 using wedeliver.Application.Features.FoodOrders.Queries.GetAllFoodOrderByRestaurantId;
+using wedeliver.Application.Features.FoodOrders.Queries.GetFoodOrderByIdClient;
 using wedeliver.Application.Features.FoodOrders.Queries.GetFoodOrderByRestaurantId;
 using wedeliver.Application.Features.FoodOrders.ViewModels;
 using wedeliver.webapi.Controllers.Base;
@@ -27,11 +28,26 @@ namespace wedeliver.webapi.Controllers.FoodOrder
            
         }
 
+        [HttpGet(("client/{clientId}/orders/{orderId}"), Name = "GetSpecificFoodOrderByClient")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<FoodOrderVM>> GetSpecificFoodOrderByClient(int clientId, int orderId)
+        {
+            var result = await Mediator.Send(new GetFoodOrderByIdClientQuery { ClientId = clientId, OrderId = orderId });
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+
+        }
+
 
         /// <summary>
         /// implement this in handler
         /// </summary>
-       
+
         [HttpPut(("status/{id}"), Name = "UpdateFoodOrderStatus")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
