1195a5fe7b24c832512c50f1f7cabe79cd8e60f8
Bumindu Yasith <bumiduyasith@gmail.com>
2021-09-20 17:53:59 +0530

    implement logic in get specific food order for restaurant

---
+++ Application/wedeliver.Application/Features/FoodOrders/Queries/GetFoodOrderByRestaurantId/GetFoodOrderByRestaurantIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wedeliver.Application.Features.FoodOrders.ViewModels;
+
+namespace wedeliver.Application.Features.FoodOrders.Queries.GetFoodOrderByRestaurantId
+{
+    public class GetFoodOrderByRestaurantIdQuery:IRequest<FoodOrderRestaurantVM>
+    {
+        public int OrderId { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
