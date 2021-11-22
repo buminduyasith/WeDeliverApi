98c6c7eca925b20b70bb9861d5158a11986b4e84
Bumindu Yasith <bumiduyasith@gmail.com>
2021-11-22 22:50:34 +0530

    pagination vm and option view models created

---
+++ Application/wedeliver.Application/Features/Resturants/Queries/ResturantsSearch/ResturantListVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wedeliver.Application.ViewModels;
+
+namespace wedeliver.Application.Features.Resturants.Queries.ResturantsSearch
+{
+    public class ResturantListVM
+    {
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int NumberOfPages { get; internal set; }
+        public int DisplayStart { get; set; }
+        public int DisplayEnd { get; set; }
+        public int DisplayCount { get; set; }
+        public IList<RestaurantVM> Lists { get; set; }
+    }
+}
---
+++ Application/wedeliver.Application/ViewModels/PaginationVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wedeliver.Application.ViewModels
+{
+    public class PaginationVM<T>
+    {
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public List<T> Results { get; set; }
+        public int NumberOfPages { get; set; }
+        public int DisplayStart { get; set; }
+        public int DisplayEnd { get; set; }
+        public int DisplayCount { get; set; }
+    }
+}
