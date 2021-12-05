7624b23f26f890f532f5f90517ac2b48361671b0
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-05 11:32:03 +0530

    user login handler file delete and add it the command file

--- Application/wedeliver.Application/Features/Login/Queries/UserLoginCommand.cs
+++ Application/wedeliver.Application/Features/Login/Queries/UserLoginCommand.cs
@@ -5,6 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using wedeliver.Application.ViewModels;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using wedeliver.Application.Contracts.Persisternce;
+
+
 
 namespace wedeliver.Application.Features.Login.Queries
 {
@@ -14,4 +20,28 @@ namespace wedeliver.Application.Features.Login.Queries
         public string Password { get; set; }
 
     }
+
+    public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, UserVM>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UserLoginCommandHandler> _logger;
+        public UserLoginCommandHandler(IUserRepository userRepository,
+
+         IMapper mapper, ILogger<UserLoginCommandHandler> logger)
+
+        {
+
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<UserVM> Handle(UserLoginCommand request, CancellationToken cancellationToken)
+        {
+            return await _userRepository.UserLogin(request.Email, request.Password);
+
+        }
+    }
+
 }
