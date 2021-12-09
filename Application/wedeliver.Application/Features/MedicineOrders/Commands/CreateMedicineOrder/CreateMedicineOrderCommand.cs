851d5109e581890cc31bd9948c3ba037510be688
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-09 22:26:20 +0530

    implement create medicine order controller and handler

---
+++ Application/wedeliver.Application/Features/MedicineOrders/Commands/CreateMedicineOrder/CreateMedicineOrderCommand.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using wedeliver.Application.Contracts.Persisternce;
+using wedeliver.Application.ViewModels;
+using wedeliver.Domain.Entities;
+using wedeliver.Domain.Enums;
+
+namespace wedeliver.Application.Features.MedicineOrders.Commands.CreateMedicineOrder
+{
+    public class CreateMedicineOrderCommand:IRequest<Boolean>
+    {
+        public string? MedsItemIntext { get; set; }
+        public string? PrescriptionUrl { get; set; }
+        public int ClientID { get; set; }
+        public string Note { get; set; }
+        public ShippingDetailsVM ShippingDetails { get; set; }
+    }
+
+    public class CreateMedicineOrderCommandHandler : IRequestHandler<CreateMedicineOrderCommand, bool>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        private readonly IMapper _mapper;
+        private readonly ILogger<CreateMedicineOrderCommandHandler> _logger;
+
+        public CreateMedicineOrderCommandHandler(
+            IMapper mapper, ILogger<CreateMedicineOrderCommandHandler> logger,
+            IApplicationDbContext context)
+
+        {
+
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _dbContext = context;
+        }
+        public async Task<bool> Handle(CreateMedicineOrderCommand request, CancellationToken cancellationToken)
+        {
+
+            var shippingDetailsModel = _mapper.Map<ShippingDetails>(request.ShippingDetails);
+
+
+            var CreatedshippingDetailsModel = await _dbContext.ShippingDetails.AddAsync(shippingDetailsModel);
+
+
+            var medicineOrder = new MedicineOrder
+            {
+                ClientID = request.ClientID,
+                EstimatedDelivery = DateTime.Now.AddDays(1),
+                OrderDate = DateTime.Now,
+                MedicineOrderStatus = MedicineOrderStatus.Pending,
+                MedsItemIntext = request.MedsItemIntext,
+                PrescriptionUrl = request.PrescriptionUrl,
+                Note = request.Note,
+                OrderType = OrderType.CashONDelivery,
+                ShippingDetails = CreatedshippingDetailsModel.Entity
+            };
+
+
+            await _dbContext.MedicineOrders.AddAsync(medicineOrder);
+
+            return true;
+        }
+    }
+}
---
+++ Application/wedeliver.webapi/Controllers/MedicineOrder/MedicineOrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wedeliver.Application.Features.MedicineOrders.Commands.CreateMedicineOrder;
+using wedeliver.webapi.Controllers.Base;
+
+namespace wedeliver.webapi.Controllers.MedicineOrder
+{
+   
+    public class MedicineOrdersController : BaseApiController
+    {
+        [HttpPost(Name = "CreateMedicineOrders")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateMedicineOrders(CreateMedicineOrderCommand request)
+        {
+            var result = await Mediator.Send(request);
+            return Ok(result);
+        }
+    }
+}
