6576a9562be94663bdc797210304bb8e0541a263
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-11 23:07:51 +0530

    remove duplicate

--- Application/wedeliver.Application/Services/EmailSenderService/EmailSenderService.cs
+++ Application/wedeliver.Application/Services/EmailSenderService/EmailSenderService.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MailKit.Net.Smtp;
-using MimeKit;
 using System.Text;
 using System.Threading.Tasks;
 using wedeliver.Application.Configurations;
