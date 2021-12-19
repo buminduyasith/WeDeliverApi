1ddc43662792370ce1b048f816fc8c92083ba7ca
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-19 11:23:11 +0530

    admin model created

---
+++ Application/wedeliver.Domain/Entities/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wedeliver.Domain.Common;
+
+namespace wedeliver.Domain.Entities
+{
+    public class Admin:EntityBase
+    {
+        public string UserId { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string PhoneNumber { get; set; }
+       
+    }
+}
