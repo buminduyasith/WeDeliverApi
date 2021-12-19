e2a3b5ed68f89fe817ed14d37e4f39809c62120f
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-19 18:54:21 +0530

    riders order controller implemented

---
+++ Application/wedeliver.webapi/Controllers/Rider/RidersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wedeliver.Application.Features.RiderOrders.Queries;
+using wedeliver.webapi.Controllers.Base;
+
+namespace wedeliver.webapi.Controllers.Rider
+{
+    public class RidersController : BaseApiController
+    {
+        [HttpPost(("orders"), Name = "GetAllPendingOrdesTobeDeliverd")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllPendingOrdesTobeDeliverd(RiderOrderQuery riderOrderQuery)
+        {
+
+            var result = await Mediator.Send(riderOrderQuery);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return NotFound();
+        }
+    }
+}
