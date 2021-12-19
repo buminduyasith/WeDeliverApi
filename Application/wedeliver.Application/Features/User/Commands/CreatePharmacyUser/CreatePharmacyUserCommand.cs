a2a2fd3f81a3476231d4f5bc923e797bea37afc1
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-19 11:22:51 +0530

    implement create pharmacy user

--- Application/wedeliver.Application/Features/User/Commands/CreatePharmacyUser/CreatePharmacyUserCommand.cs
+++ Application/wedeliver.Application/Features/User/Commands/CreatePharmacyUser/CreatePharmacyUserCommand.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using wedeliver.Application.Contracts.Persisternce;
 using wedeliver.Domain.Enums;
 
 namespace wedeliver.Application.Features.User.Commands.CreatePharmacyUser
 {
-    public class CreatePharmacyUserCommand
+    public class CreatePharmacyUserCommand:IRequest
     {
         public string Email { get; set; }
         public string Password { get; set; }
@@ -24,8 +29,28 @@ namespace wedeliver.Application.Features.User.Commands.CreatePharmacyUser
 
     }
 
-    public class CreatePharmacyUserCommandHandler
+    public class CreatePharmacyUserCommandHandler : IRequestHandler<CreatePharmacyUserCommand, Unit>
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CreatePharmacyUserCommandHandler> _logger;
 
+        public CreatePharmacyUserCommandHandler(IUserRepository userRepository,
+
+            IMapper mapper, ILogger<CreatePharmacyUserCommandHandler> logger)
+
+        {
+
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(CreatePharmacyUserCommand request, CancellationToken cancellationToken)
+        {
+            await _userRepository.CreatePharmacyUser(request);
+
+            return await Task.FromResult(Unit.Value);
+        }
     }
 }
