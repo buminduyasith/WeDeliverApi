d4a28696363c4bf782f5c8b11e136896a6856483
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-19 18:55:23 +0530

    GetMedicineOrders controller implemented

--- Application/wedeliver.webapi/Controllers/MedicineOrder/MedicineOrdersController.cs
+++ Application/wedeliver.webapi/Controllers/MedicineOrder/MedicineOrdersController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using wedeliver.Application.Features.MedicineOrders.Commands.CreateMedicineOrder;
+using wedeliver.Application.Features.MedicineOrders.Queries.GetMedicineOrdersQuery;
+using wedeliver.Application.ViewModels;
 using wedeliver.webapi.Controllers.Base;
 
 namespace wedeliver.webapi.Controllers.MedicineOrder
@@ -16,6 +18,16 @@ namespace wedeliver.webapi.Controllers.MedicineOrder
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateMedicineOrders(CreateMedicineOrderCommand request)
+        {
+            var result = await Mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpPost("orders",Name = "GetMedicineOrders")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<MedicineOrderVM>>> GetMedicineOrders(GetMedicineOrdersQuery request)
         {
             var result = await Mediator.Send(request);
             return Ok(result);
