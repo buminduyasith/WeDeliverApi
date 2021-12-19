a65aba84244779ac1d8b0d6df823071ecddd075d
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-19 11:26:23 +0530

    minor update for project

---
+++ Application/wedeliver.Infrastructure/Persistence/Extensions/Seeds/ClientAccountsSeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wedeliver.Domain.Entities;
+
+namespace wedeliver.Infrastructure.Persistence.Extensions.Seeds
+{
+    public static class ClientAccountsSeed
+    {
+        public static void SeedCientAccounts(this ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Client>().HasData(
+
+            //    new Client {FName="bumindu",LName="yasith" }
+                
+            //    )
+        }
+    }
+}
