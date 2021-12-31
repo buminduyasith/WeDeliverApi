b1deac011e83f83b0ab14b4257b2783b8babc194
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-31 16:06:40 +0530

    pdftesting

--- Application/wedeliver.webapi/Controllers/FoodCategory/FoodCategoryController.cs
+++ Application/wedeliver.webapi/Controllers/FoodCategory/FoodCategoryController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using wedeliver.Application.Features.Foods.Commands.AddFoodCategory;
 using wedeliver.Application.Features.Foods.Queries.GetAllFoodCategory;
+using wedeliver.Application.Features.Foods.Queries.TestPdf;
 using wedeliver.Application.ViewModels;
 using wedeliver.webapi.Controllers.Base;
 
@@ -37,6 +38,22 @@ namespace wedeliver.webapi.Controllers.FoodCategory
             }
 
             return NotFound();
+
+        }
+
+        [HttpGet("/test",Name = "wow")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ContentResult> test()
+        {
+
+            var result = await Mediator.Send(new TestPdfQuery());
+            if (result != null)
+            {
+                return base.Content(result, "text/html");
+            }
+
+            return base.Content("<div>Hello</div>", "text/html");
 
         }
     }
