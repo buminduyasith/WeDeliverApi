e92671219df0092e7aaff14b78996f6e0c1115e0
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-31 16:05:43 +0530

    location extension created

---
+++ Application/wedeliver.Application/Extensions/LocationExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wedeliver.Domain.Entities;
+
+namespace wedeliver.Application.Extensions
+{
+    public static class LocationExtension
+    {
+        public static string formattedLocation(this Location location)
+        {
+            return $"{location.HouseNo}";
+        }
+    }
+}
