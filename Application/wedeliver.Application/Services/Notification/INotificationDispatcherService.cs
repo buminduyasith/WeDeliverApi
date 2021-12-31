becc2364c433ebdc4b39cfa8b05be1b8cea43748
Bumindu Yasith <bumiduyasith@gmail.com>
2021-12-31 16:11:19 +0530

    notificationdispatcher created

---
+++ Application/wedeliver.Application/Services/Notification/INotificationDispatcherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wedeliver.Application.Services.Notification
+{
+    public interface INotificationDispatcherService
+    {
+        Task SendEmail(string templateTag,
+           string[] toEmails,
+           Dictionary<string, string> data,
+           Dictionary<string, string> attachments = null);
+    }
+}
