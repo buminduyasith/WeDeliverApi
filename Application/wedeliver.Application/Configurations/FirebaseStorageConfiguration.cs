c1e4f9f7cc97ba0e693b7fbb2c65e6cb128949da
Bumindu Yasith <bumiduyasith@gmail.com>
2022-01-08 12:08:30 +0530

    add firebaseStorage configuration

---
+++ Application/wedeliver.Application/Configurations/FirebaseStorageConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wedeliver.Application.Configurations
+{
+    public class FirebaseStorageConfiguration
+    {
+        public string ApiKey { get; set; }
+        public string Bucket { get; set; }
+    }
+}
